dedcb7595ac5e42606193b0bd3978321e04d79b1
soral215dev <soral215@storelink.io>
2021-01-14 18:08:34 +0900

    Antd 설치 및 데이터 받아오는 작업 중

---
+++ src/views/apps/test/Test.vue
@@ -0,0 +1,75 @@
+<template>
+  <a-table :columns="columns" :data-source="data">
+    <a slot="name" slot-scope="text">{{ text }}</a>
+  </a-table>
+</template>
+<script>
+import firebase from 'firebase'
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    scopedSlots: { customRender: 'name' },
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+    width: 80,
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address 1',
+    ellipsis: true,
+  },
+  {
+    title: 'Long Column Long Column Long Column',
+    dataIndex: 'address',
+    key: 'address 2',
+    ellipsis: true,
+  },
+  {
+    title: 'Long Column Long Column',
+    dataIndex: 'address',
+    key: 'address 3',
+    ellipsis: true,
+  },
+  {
+    title: 'Long Column',
+    dataIndex: 'address',
+    key: 'address 4',
+    ellipsis: true,
+  },
+];
+
+const data = [];
+
+export default {
+  data() {
+    return {
+      data,
+      columns
+    };
+  },
+  
+ methods: {
+ pushData() {
+ 
+  data.push()
+
+ }
+
+ },
+ mounted () {  
+ const db = firebase.database().ref("user").limitToLast(10);
+      db.on("value", snapshot => {
+      snapshot.val();
+      console.log(snapshot.val());
+      })
+}
+
+  
+};
+</script>
